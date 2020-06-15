6ae3342344c05806f7a79a85d939ee2b8514cc46
Yosa <yosa@yosas-mbp.proxy3.pens.ac.id>
2020-06-15 19:18:50 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna/>
+    <HerroShayna/>
+    <WomanShayna/>
+    <InstaShayna/>
+    <PartnerShayna/>
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HerroShayna from '@/components/HerroShayna.vue'
+import WomanShayna from '@/components/WomanShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HerroShayna,
+    WomanShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna,
+  }
+};
+</script>
